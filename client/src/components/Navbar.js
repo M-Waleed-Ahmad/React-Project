8ef497d6f7d1e2570d4a9413155dea0261ac042d
Nox <waleed.ahmadmunir@gmail.com>
2024-05-26 01:27:20 +0500

    Responsive  Navbar Created

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState } from 'react';
+
+import logo from '../assets/logo.jpg';
+const Navbar =()=>  {
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    const toggleSidebar = () => {
+      setShowSidebar(!showSidebar);
+      document.querySelector('.Sidebar').classList.toggle('show');
+    };
+return (
+    <>
+<div className="navbar">
+    <div className="div1">
+      <img src={logo} alt="Logo" className="logo" height="50px" width="50px"></img>
+      <a href="/" className="website-name">Nexus Medica</a>
+    </div>
+    <div className="div2">
+      <div className="nav-links">
+        <a href="/home">Home</a>
+        <a href="/services">Services</a>
+        <a href="/doctors">Doctors</a>
+        <a href="/contact">Contact Us</a>
+        <a href="/ask" className="signup-logout">Signup / Logout</a>
+
+      </div>
+
+    </div>
+      {/* Hamburger menu */}
+      <div className="hamburger-menu" onClick={toggleSidebar}>
+            <div className={`bar ${showSidebar ? 'open' : ''}`}></div>
+            <div className={`bar ${showSidebar ? 'open' : ''}`}></div>
+            <div className={`bar ${showSidebar ? 'open' : ''}`}></div>
+    </div> 
+    <div className="Sidebar">
+            <a href="/home">Home</a>
+            <a href="/services">Services</a>
+            <a href="/doctors">Doctors</a>
+            <a href="/contact">Contact Us</a>
+            <a href="/ask" className="signup-logout">Signup / Logout</a>
+    </div>
+  </div>
+  
+    </>
+)
+}
+
+export default Navbar
---
+++ client/src/pages/CoverPage.js
@@ -0,0 +1,13 @@
+// src/pages/CoverPage.js
+
+import React from 'react';
+import Navbar from '../components/Navbar';
+function CoverPage() {
+  return (
+    <>
+    <Navbar />
+    </>
+  );
+}
+
+export default CoverPage;
